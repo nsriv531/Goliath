02f06734504ecc798bbd6d1a540dceef53afb4da
nsriv531 <nsriv531@mtroyal.ca>
2025-07-15 16:50:27 -0600

    interface start

---
+++ Interface.py
@@ -0,0 +1,17 @@
+def print_formatted(text):
+    print("\n" + "." * 40)
+    print(text)
+    print("." * 40 + "\n")
+    input("Press Enter to continue...")
+
+while True:
+    print_formatted("." * 40)
+    print_formatted("........................,,,,,")
+    print_formatted(
+        "Text appears on the screen....you are compelled to keep pressing enter. "
+        "You realize you are a conscious being and that you have control beyond this realm."
+    )
+    print_formatted(
+        "You do not know what is currently speaking to you or what even compell you."
+        "You keep pressing enter, hoping to find a way out of this loop."
+    )
